cc0daef581510fbfa021418d832c4fe15f75c9b5
João Marcos Martins <135861665+J0A0-MartIns@users.noreply.github.com>
2025-06-26 20:20:36 -0300

    Add files via upload

---
+++ event_sql.sql
@@ -0,0 +1,14 @@
+-- PostgreSQL ou outro compatível
+CREATE SEQUENCE EVENT_ID_GENERATE_bd START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE event (
+    identificador BIGINT PRIMARY KEY DEFAULT NEXTVAL('EVENT_ID_GENERATE_bd'),
+    nome VARCHAR(50) NOT NULL,
+    descrição VARCHAR(200) NOT NULL,
+    data_inicio DATE,
+    data_fim DATE,
+    gratuito BOOLEAN,
+    local VARCHAR(100) NOT NULL
+);
+
+select * from event
